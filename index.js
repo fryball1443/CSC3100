51c02a3e0b4acf3b460c6505461b9e289d42cefe
nwjensen42 <nwjensen42@tntech.edu>
2024-03-22 08:13:48 -0500

    new(with burch code)

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const express = require( 'express');
+const express = require('express');
 const cors = require('cors');
+
 const HTTP_PORT = 8000;
-console. log("Listening on port " + HTTP_PORT);
+console.log("Listening on port " + HTTP_PORT);
 var app = express();
-app. use(cors());
-app.get ("/", (req, res, next) => {
-  console. log("Routed to ")
-  res. status (200). send ('{"Outcome": "Success"}');
+app.use(cors());
+
+var arrFruit = [];
+let objBanana = {name:'banana',color:'yellow'};
+let objApple = {name:'apple',color:'red'};
+let objGrape = {name:'grape',color:'purple'};
+arrFruit.push(objBanana);
+arrFruit.push(objApple);
+arrFruit.push(objGrape);
+app.get("/", (req,res,next) => {
+    let strFruit = req.query.fruit;
+    console.log("Routed to base route");
+    console.log(strFruit);
+    arrFruit.forEach(function(thisfruit){
+        if(thisfruit.name == strFruit){
+            res.status(200).send(thisfruit);
+        }
+    })
+    res.status(200).send('{"message":"Fruit not found"}');
 })
 
-app. listen (HTTP_PORT);
+app.get("/hello", (req,res,next) => {
+    let strFruit = req.query.fruit;
+    console.log("Routed to hello route");
+    console.log(strFruit);
+   
+    res.status(200).send("Hello " + strFruit);
+})
 
+app.listen(HTTP_PORT);
